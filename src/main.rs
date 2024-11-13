26c46202e4dadd074cc12d4519448eebbc184035
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2024-11-13 09:31:04 -0300

    Read inputs and check hash file existence

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::path::Path;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,13 +10,59 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+
+    match args[1].as_str() {
+        "init" => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+        "cat-file" => {
+            // TODO: Check if working dir was git init
+
+            // Note -p is a flag, so it might be optional and at any position
+            // refactor this later to better handle flags and optional flags
+            assert_eq!(args.len(), 4, "usage: cat-file -p <hash>");
+            assert_eq!(args[2], "-p", "usage: cat-file -p <hash>");
+            let hash = &args[3];
+            assert_eq!(hash.len(), 40, "Hash size invalid (must be 40 chars long)");
+
+            const GIT_OBJECTS_PATH: &str = ".git/objects";
+            // let hashPath = GIT_OBJECTS_PATH + hash[:2] +"/"+ hash[2:];
+
+            // STUDY: Investigate/Understand why this won't work without the &
+            // let hashPath = format!(
+            //     "{}/{}/{}",
+            //     GIT_OBJECTS_PATH,
+            //     hash[0..2],
+            //     hash[2..hash.len()]
+            // );
+
+            // STUDY: Why this won't generate an issue? (what if hash provide has length == 1?)
+            let hash_path = format!("{}/{}/{}", GIT_OBJECTS_PATH, &hash[0..2], &hash[2..]);
+            println!("{}", hash_path);
+
+            //check file existence
+            let file = Path::new(&hash_path);
+            if !file.exists() {
+                // Panic is probably not the best way.
+                // Check if theres a better way (write to sdterr and exit(1) or something)
+                // Also, check what Panic does, and if I should avoid it
+                panic!("File doesn't exist")
+            }
+
+            // load data from file
+
+            // decompress it Zlib/flate2
+
+            // extract type, length and content
+
+            // print content
+            let content = "";
+            print!("{}", content)
+        }
+        _ => println!("unknown command: {}", args[1]),
     }
 }
